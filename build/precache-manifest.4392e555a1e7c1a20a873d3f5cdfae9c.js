066d2e6106122d496aa521745851c706def62794
RegnaR19 <nar200@yandex.ru>
2022-11-09 21:56:06 +0500

    s

---
+++ build/precache-manifest.4392e555a1e7c1a20a873d3f5cdfae9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b17142bbfcc7ea272fa",
+    "url": "/alpha-studio/static/css/main.1f15d496.chunk.css"
+  },
+  {
+    "revision": "8b17142bbfcc7ea272fa",
+    "url": "/alpha-studio/static/js/main.62c7bd62.chunk.js"
+  },
+  {
+    "revision": "3c3a91e0579a28d9f621",
+    "url": "/alpha-studio/static/js/runtime~main.9ef7d649.js"
+  },
+  {
+    "revision": "5370114cd20f8ad511f5",
+    "url": "/alpha-studio/static/css/2.eb98df2b.chunk.css"
+  },
+  {
+    "revision": "5370114cd20f8ad511f5",
+    "url": "/alpha-studio/static/js/2.5bbffa31.chunk.js"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/alpha-studio/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/alpha-studio/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "1b19635fdac89c92087aa8d5397d51e1",
+    "url": "/alpha-studio/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/alpha-studio/precache-manifest.4392e555a1e7c1a20a873d3f5cdfae9c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/alpha-studio/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
